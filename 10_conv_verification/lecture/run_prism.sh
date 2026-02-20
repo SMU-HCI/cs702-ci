dbe55ce4fead29920e35e7d469a9b3c4db7077ad
Kotaro <koe.bluebear@gmail.com>
2026-02-20 10:36:27 +0000

    Add a comment

--- 10_conv_verification/lecture/run_prism.sh
+++ 10_conv_verification/lecture/run_prism.sh
@@ -3,6 +3,8 @@
 # To run the following commands, make sure you have the PRISM model checker installed and available in your PATH.
 # For this, you can run `bash install_prism.sh` from the root of the repository.
 
+# To verify a specific property, run prism from command line (e.g., `prism engagement.pm -pf 'P=? [ F "converted" ]'`)
+
 # DTMC model: engagement.pm
 prism engagement.pm engagement_reachability.pctl
 prism engagement.pm engagement_invariance.pctl
